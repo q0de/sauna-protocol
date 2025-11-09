5bac5c3aeaccf237dc6983bc8e845eea3435111d
Michael Grande <michaelagrande@gmail.com>
2025-11-09 17:27:59 -0600

    Add Plausible analytics for privacy-friendly tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import "./globals.css"
 import { Header } from "@/components/layout/header"
 import { Footer } from "@/components/layout/footer"
@@ -65,6 +66,18 @@ export default function RootLayout({
           <Footer />
         </div>
         <Analytics />
+        
+        {/* Privacy-friendly analytics by Plausible */}
+        <Script
+          src="https://plausible.io/js/pa-2-WGIOmu4gHTzs_q5qyUX.js"
+          strategy="afterInteractive"
+        />
+        <Script id="plausible-init" strategy="afterInteractive">
+          {`
+            window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+            plausible.init()
+          `}
+        </Script>
       </body>
     </html>
   )
